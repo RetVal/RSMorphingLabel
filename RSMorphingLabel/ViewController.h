207389fbcc850fae408b9ab21c6d63c3482c251f
closure <retval@me.com>
2014-07-20 22:24:33 +0800

    Initial Commit

---
+++ RSMorphingLabel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RSMorphingLabel
+//
+//  Created by closure on 7/20/14.
+//  Copyright (c) 2014 closure. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
